53fdf849249ae3de8cf993d4888632ab5145ec4a
Saikat-D023 <saikatdey358@gmail.com>
2025-03-06 23:55:44 +0530

    added

--- app/api/auth/[...modules]/route.ts
+++ app/api/auth/[...modules]/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export function GET() {
     return NextResponse.json({
-        message : "catch all"
+        message : "catch all statement"
     })
 }
